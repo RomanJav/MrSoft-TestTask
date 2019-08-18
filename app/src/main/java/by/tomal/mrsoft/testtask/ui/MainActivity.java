6e20df247089c429e19c301fe8bc7de344a12cab
RomanJav <tomal.roman94@gmail.com>
2019-08-18 18:58:05 +0300

    Add filter for searching

--- app/src/main/java/by/tomal/mrsoft/testtask/ui/MainActivity.java
+++ app/src/main/java/by/tomal/mrsoft/testtask/ui/MainActivity.java
@@ -1,16 +1,23 @@
 package by.tomal.mrsoft.testtask.ui;
 
+
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Button;
 import android.widget.RadioButton;
-import android.widget.SearchView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import by.tomal.mrsoft.testtask.R;
 import by.tomal.mrsoft.testtask.presenter.LoadUserList;
 import by.tomal.mrsoft.testtask.presenter.SortUserList;
@@ -21,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private UserListAdapter userListAdapter = new UserListAdapter();
     private RecyclerView userList;
     private Button sortUsersByButton;
-    private SearchView searchUser;
+    private Toolbar toolbar;
     private RadioButton checkSortByName;
     private RadioButton checkSortByLevel;
 
@@ -39,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private void initLayout() {
         userList = findViewById(R.id.user_list);
         sortUsersByButton = findViewById(R.id.sort_list_button);
-        searchUser = findViewById(R.id.search_view);
+        toolbar = findViewById(R.id.search_view);
         checkSortByName = findViewById(R.id.sort_by_name);
         checkSortByLevel = findViewById(R.id.sort_by_level);
     }
@@ -48,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         userList.setHasFixedSize(true);
         userList.setLayoutManager(new LinearLayoutManager(this));
         userList.setAdapter(userListAdapter);
+        setSupportActionBar(toolbar);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -58,9 +66,34 @@ public class MainActivity extends AppCompatActivity {
 
     private void sortUsers() {
         SortUserList users = new SortUserList(userListAdapter);
-        if(checkSortByName.isChecked())
+        if (checkSortByName.isChecked())
             users.sortByName();
-        else if(checkSortByLevel.isChecked())
+        else if (checkSortByLevel.isChecked())
             users.sortByLevel();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                userListAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                userListAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/by/tomal/mrsoft/testtask/ui/userListAdapter/UserListAdapter.java
+++ app/src/main/java/by/tomal/mrsoft/testtask/ui/userListAdapter/UserListAdapter.java
@@ -3,6 +3,8 @@ package by.tomal.mrsoft.testtask.ui.userListAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,14 +16,16 @@ import java.util.List;
 import by.tomal.mrsoft.testtask.R;
 import by.tomal.mrsoft.data.entity.User;
 
-public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> {
+public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> implements Filterable {
 
     private List<User> userList = new ArrayList<>();
+    private List<User> userListPreFiltered = new ArrayList<>();
 
     @NonNull
     @Override
     public UserListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_list_item, parent, false);
+        view.setOnClickListener(sort -> sortUsersByRate());
         return new UserListViewHolder(view);
     }
 
@@ -37,6 +41,7 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> {
 
     public void setNewUsers(List<User> users){
         userList = users;
+        userListPreFiltered = users;
         notifyDataSetChanged();
     }
 
@@ -48,5 +53,39 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> {
     public void sortUsersByName(){
         Collections.sort(userList, (user1, user2) -> user1.getName().compareToIgnoreCase(user2.getName()));
         notifyDataSetChanged();
+    }
+    private void sortUsersByRate(){
+        Collections.sort(userList, (user1, user2) -> Integer.valueOf(user1.getRate()).compareTo(Integer.valueOf(user2.getRate())));
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter(){
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    userList = userListPreFiltered;
+                } else {
+                    List<User> filteredList = new ArrayList<>();
+                    for (User user : userList) {
+                        if (user.getName().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(user);
+                        }
+                    }
+                    userList = filteredList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = userList;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                userList = (ArrayList<User>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
